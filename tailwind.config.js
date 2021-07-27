55d5ecb4fde590f315543aebb09b010a774c3231
Nashir Jamali <nashirjamali@gmail.com>
2021-07-27 21:15:26 +0700

    change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/**/*'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/**/*"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ["'Josefin Sans'"],
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
